1373ebc0fd836a36e06363c657f8b160fe13223d
ajaytidke28297 <ajaytidke28297@gmail.com>
2021-11-16 16:26:50 +0530

    ask questions till the question bank is exhausted

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from question_model import Question
+from quiz_brain import QuizBrain
 from data import question_data
 
 question_bank = []
@@ -6,4 +7,7 @@ question_bank = []
 for question in question_data:
     question_bank.append(Question(question['text'], question['answer']))
 
-print(question_bank)
+quiz_brain = QuizBrain(question_bank)
+
+while quiz_brain.still_has_questions():
+    quiz_brain.next_question()
